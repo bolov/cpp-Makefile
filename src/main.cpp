7e271bf5c7d656a03ad1127a7891ab05e376f532
Mihail Bălan <mihail.balan@gmail.com>
2015-04-22 18:47:14 +0300

    added another compile unit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+#include "required.hpp"
+
 using namespace std;
 
 int main() {
 
   cout << "Welcome and wake up" << endl;
+  cout << "The answer is " << the_answer_to_life_the_universe_and_everything()
+       << "." << endl;
+  cout << "You are welcome" << endl;
 
   return 0;
 }
